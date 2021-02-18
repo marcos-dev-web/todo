bc414f87c7ea73fcabca5ac5c9fd03a327847a1e
marcos-dev-web <marcospuglliesyprogramer@gmail.com>
2021-02-18 16:46:26 -0300

    refatored javascript

--- js/main.js
+++ js/main.js
@@ -1,92 +1,113 @@
-const todoContainer = document.querySelector('.list_todo_items');
-const buttonDone = document.querySelector('#done_make');
-const inputText = document.querySelector('input#todo_name');
+const todoContainer = document.querySelector(".list_todo_items");
+const buttonDone = document.querySelector("#done_make");
+const inputText = document.querySelector("input#todo_name");
+
+const ce = (e) => {
+  const element = document.createElement(e);
+  return {
+    addClass(className) {
+      element.classList.add(className);
+    },
+    append(el) {
+      element.appendChild(el);
+    },
+    listen(eventName, func) {
+      element.addEventListener(eventName, func);
+    },
+    text(txt) {
+      element.textContent = txt;
+    },
+    el: element,
+  };
+};
 
 const todo = {
   create(text) {
-    let containerTodo = document.createElement('li'); // li.item_todo
-    let containerText = document.createElement('p'); // p.text
-    let containerButtons = document.createElement('div') // div.buttons
-    let buttonTrash = document.createElement('button') // button.done.trash
-    let tagTrash = document.createElement('i') // i.fas.fa-trash
+    let containerTodo = ce("li"); // li.item_todo
+    let containerText = ce("p"); // p.text
+    let containerButtons = ce("div"); // div.buttons
+    let buttonTrash = ce("button"); // button.done.trash
+    let tagTrash = ce("i"); // i.fas.fa-trash
 
     //add classes
-    containerTodo.classList.add('item_todo');
-    containerText.classList.add('text');
-    containerButtons.classList.add('buttons');
-    buttonTrash.classList.add('done');
-    buttonTrash.classList.add('trash');
-    tagTrash.classList.add('fas');
-    tagTrash.classList.add('fa-trash');
+    containerTodo.addClass("item_todo");
+    containerText.addClass("text");
+    containerButtons.addClass("buttons");
+    buttonTrash.addClass("done");
+    buttonTrash.addClass("trash");
+    tagTrash.addClass("fas");
+    tagTrash.addClass("fa-trash");
 
     //add content
-    containerText.textContent = text;
+    containerText.text(text);
 
     //encapsulation
-      //container buttons
-      buttonTrash.appendChild(tagTrash);
-      containerButtons.appendChild(buttonTrash);
-      //container li item_todo
-      containerTodo.appendChild(containerText);
-      containerTodo.appendChild(containerButtons);
-    
+    //container buttons
+    buttonTrash.append(tagTrash.el);
+    containerButtons.append(buttonTrash.el);
+    //container li item_todo
+    containerTodo.append(containerText.el);
+    containerTodo.append(containerButtons.el);
+
     //add Event
     function del() {
-      todo.remove(containerTodo);
+      todo.remove(containerTodo.el);
     }
-    buttonTrash.addEventListener('click', del)
-    return containerTodo;
+    buttonTrash.listen("click", del);
+    return containerTodo.el;
   },
   render(todo) {
-    todoContainer.appendChild(todo);
+    todoContainer.append(todo);
   },
   remove(todoDiv) {
-    todoDiv.style.transform = 'rotate(10deg)';
-    todoDiv.style.opacity = '0.5';
-    todoDiv.style.left = '10px';
+    todoDiv.style.transform = "rotate(10deg)";
+    todoDiv.style.opacity = "0.5";
+    todoDiv.style.left = "10px";
     setTimeout(() => {
       const parent = todoDiv.parentNode;
       parent.removeChild(todoDiv);
-      todo.minCounter()
-    }, 250)
+      todo.downCounter();
+    }, 250);
   },
-  plusCounter() {
-    const divNumber = document.querySelector('#tot_number');
+  upCounter() {
+    const divNumber = document.querySelector("#tot_number");
     const currentValue = Number.parseInt(divNumber.textContent);
     divNumber.innerText = currentValue + 1;
   },
-  minCounter() {
-    const divNumber = document.querySelector('#tot_number');
+  downCounter() {
+    const divNumber = document.querySelector("#tot_number");
     const currentValue = Number.parseInt(divNumber.textContent);
     divNumber.innerText = currentValue - 1;
   },
-  error(text) {
-    const alertDiv = document.querySelector('.alert');
-    alertDiv.querySelector('.msg').innerText = text;
+  alertMsg(text) {
+    const alertDiv = document.querySelector(".alert");
+    alertDiv.querySelector(".msg").innerText = text;
     alertDiv.style.top = 0;
     setTimeout(() => {
-      alertDiv.removeAttribute('style');
-    }, 3000)
-  }
-}
+      alertDiv.removeAttribute("style");
+    }, 3000);
+  },
+};
 
 function newTd() {
-  const inputText = document.querySelector('input#todo_name');
+  const inputText = document.querySelector("input#todo_name");
   const inputValue = inputText.value;
   if (inputValue) {
     const newTodo = todo.create(inputValue);
     todo.render(newTodo);
-    inputText.value = '';
-    todo.plusCounter();
-    inputText.focus()
+    inputText.value = "";
+    todo.upCounter();
+    inputText.focus();
   } else {
-    todo.error('field todo is empty')
+    todo.alertMsg("field todo is empty");
   }
 }
 
-buttonDone.addEventListener('click', newTd);
-inputText.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
-    newTd();
-  }
-})
+const actions = {
+  Enter: newTd,
+};
+
+buttonDone.addEventListener("click", newTd);
+inputText.addEventListener("keydown", (event) => {
+  actions[event.key] && actions[event.key]();
+});
